c7f56d8a5c2e4717a1fefb0f4dd6ed2b8d11a54a
PictureBookGraduate <smora050@mtroyal.ca>
2019-11-07 15:49:32 -0700

    Add about page

---
+++ about.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>  
+        <title>About page</title>   
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,800" rel="stylesheet">    
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <main class="container">
+            <div class="grid-container"> 
+
+                <!-- Description of the Site -->
+                <h1>COMP_3512 Web Development II (Fall 2019)</h1>
+                <h3>Mount Royal University</h3>
+                <h3>Professor: Randy Connolly</h3>
+                <p>Technologies: HTML, CSS, PHP, JS, and GIT</p>
+                <h4>Group Members:</h4>
+                <ul>
+                    <li>Peter M.</li>
+                    <li>Yuna S.</li>
+                    <li>Stefan M.</li>
+                </ul>
+                <a href="https://github.com/StefanKristoff/Web2Assignment2.git"> Main github page link</a>
+
+            </div> 
+        </main>   
+    </body>
+</html>
