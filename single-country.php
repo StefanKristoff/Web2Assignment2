a1e3f2279b1d626811a41c8874c6577159ad2a02
PictureBookGraduate <smora050@mtroyal.ca>
2019-11-12 20:06:27 -0700

    Created basic outline

--- single-country.php
+++ single-country.php
@@ -1,12 +1,37 @@
+<?php
+    
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title></title>
     </head>
     <body>
-        <?php
+        <div class='grid-container'>
+            <header> 
+                <h1></h1>
+                <h4></h4>
+            </header>
+            <main> 
+                <div class='box a'>
+
+                </div>
+                <div class='box b'>
+
+                </div>
+                <div class='box c'>
+
+                </div>
+                <div class='box d'>
+
+                </div>
+                <div class='box e'>
+
+                </div>
+            </main>
+            
+        </div>
         
-        
-        ?>
     </body>
 </html>
