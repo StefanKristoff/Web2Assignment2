685df5736c44e436bac02205bee66e60105eb6dd
ysong299 <yunaleska331@gmail.com>
2019-11-13 22:42:33 -0700

    added onto single-photo.php

--- single-photo.php
+++ single-photo.php
@@ -1,12 +1,53 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <?php
-        
-        
-        ?>
-    </body>
+
+<head>
+    <title>Single Photo</title>
+    <!-- <link rel="stylesheet" href="stylesheet.css"> -->
+    <link rel="stylesheet" href="single-photo.css">
+</head>
+
+<body>
+    <main class='grid-container'>
+        <div class="box h">
+            <header>
+                <h1>Header</h1>
+            </header>
+        </div>
+        <section class="box singleView">
+            <div id="singlePic">
+                <img src="fake.jpg" width="300" height="300">
+            </div>
+            <div id="singlePicInfo">
+                <div id="singlePicDetails">
+                    <label>Photo Title:</label>
+                    <span id="photoTitle"></span>
+                    </br></br>
+                    <label>User Name:</label>
+                    <span id="username"></span>
+                    </br></br>
+                    <label>Country, City:</label>
+                    <span id="countryCity"></span>
+
+                    <div id="descriptionsTab">
+                        <button class="itemTab" id="tabDescription">Description</button>
+                        <button class="itemTab" id="tabDetails">Details</button>
+                        <button class="itemTab" id="tabMap">Map</button>
+                        <div class="tabBox" id="tabBoxDescription">
+                            <!-- <img src="fake.img" width="300" height="300" > -->
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </section>
+    </main>
+
+    <?php
+
+
+    ?>
+</body>
+
 </html>
