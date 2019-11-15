358bffb25e52107c2cf7c1da11c579405044b0a8
ysong299 <yunaleska331@gmail.com>
2019-11-15 00:31:21 -0700

    Shortened hamburger code to a more efficient way

--- includes/hamburger.inc.php
+++ includes/hamburger.inc.php
@@ -1,23 +1,11 @@
 <?php
 function createHamburger()
 {
+    $pages = ["about", "browser", "favourites", "index-logged-in", "index", "single-city", "single-country", "single-photo"];
     echo "<div class='hamburgerNav'>";
-
-    echo "<a href='http://localhost/Web2Assignment2/about.php'>About Page</a>";
-    echo "</br>";
-    echo "<a href='http://localhost/Web2Assignment2/browser.php'>Browser</a>";
-    echo "</br>";
-    echo "<a href='http://localhost/Web2Assignment2/favourites.php'>Favourites</a>";
-    echo "</br>";
-    echo "<a href='http://localhost/Web2Assignment2/index-logged-in.php'>Index Page When Logged In</a>";
-    echo "</br>";
-    echo "<a href='http://localhost/Web2Assignment2/index.php'>Index Page</a>";
-    echo "</br>";
-    echo "<a href='http://localhost/Web2Assignment2/single-city.php'>Single City</a>";
-    echo "</br>";
-    echo "<a href='http://localhost/Web2Assignment2/single-country.php'>Single Country</a>";
-    echo "</br>";
-    echo "<a href='http://localhost/Web2Assignment2/single-photo.php'>Single Photo</a>";
-
+    foreach($pages as $p){
+        echo "<a href='http://localhost/Web2Assignment2/$p.php'>$p page</a>";
+        echo "</br>";
+    }
     echo "</div>";
 }
