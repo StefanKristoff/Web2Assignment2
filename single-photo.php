33c001c4481539483ad84fa3c8d06457bea5ec98
ysong299 <yunaleska331@gmail.com>
2019-11-14 22:08:15 -0700

    Created the favorite button and positioned it

--- single-photo.php
+++ single-photo.php
@@ -19,18 +19,15 @@ include('includes\header.inc.php');
             </div>
             <div id="singlePicInfo">
                 <div id="singlePicDetails">
-                    <label>Photo Title:</label>
-                    <span id="photoTitle"></span>
-                    </br></br>
-                    <label>User Name:</label>
-                    <span id="username"></span>
-                    </br></br>
-                    <label>Country, City:</label>
-                    <span id="countryCity"></span>
-
                     <div id="favoritesButton">
-                        <button id="favorites">Favorites</button>
+                        <button id="favorites">Add to Favorites</button>
                     </div>
+                    <span id="photoTitle">Photo Title:</span>
+                    </br></br>
+                    <span id="username">User Name:</span>
+                    </br></br>
+                    <span id="countryCity">Country, City:</span>
+
                     <div id="descriptionsTab">
                         <button class="itemTab" id="tabDescription">Description</button>
                         <button class="itemTab" id="tabDetails">Details</button>
