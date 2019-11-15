7ad7432f874018b425e514fcef70989ad673990c
Peter Morrison <morrisonman56@gmail.com>
2019-11-14 23:49:41 -0700

    Add browser functionality

--- browser.php
+++ browser.php
@@ -1,13 +1,22 @@
 <?php
 include('includes\header.inc.php');
+
+function createResultRow($photo, $title) {
+    echo "<div class='resultrow card'>";
+    echo "<img src='$photo' width='100px' height='100px'/>";
+    echo "<p>$title</p>";
+    echo "<button>View</button>";
+    echo "<button>Add to Favourites</button>";
+    echo "</div>";
+}
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>Single Photo</title>
+    <title>Browse Photos</title>
     <link rel="stylesheet" href="css\stylesheet.css">
-    <link rel="stylesheet" href="css\single-photo.css">
+    <link rel="stylesheet" href="css\browser.css">
 </head>
 
 <body>
@@ -16,20 +25,19 @@ include('includes\header.inc.php');
         <div class='box a card'>
             <h3>Country Filter</h3>
         </div>
-        <div class='box b card'>
-            <h3>Country List</h3>
-        </div>
 
-        <section>
-            <div class='box c card'>
-                <h3>Country Details</h3>
-            </div>
-            <div class='box d card'>
-                <h3>City List</h3>
-            </div>
-            <div class='box e card'>
-                <h3>Country Photos</h3>
-            </div>
+        <section class="box b card">
+            <?= createResultRow("FakeImg.jpg", "Title1"); ?>
+            <?= createResultRow("FakeImg.jpg", "Title2"); ?>
+            <?= createResultRow("FakeImg.jpg", "Title3"); ?>
+            <?= createResultRow("FakeImg.jpg", "Title4"); ?>
+            <?= createResultRow("FakeImg.jpg", "Title5"); ?>
+            <!-- <div class="resultrow">
+                <img src='fakeimg.jpg' width='100px' height='100px'/>
+                <p>Title</p>
+                    <button>View</button>
+                    <button>Add to Favourites</button>
+            </div> -->
         </section>
 
     </main>
