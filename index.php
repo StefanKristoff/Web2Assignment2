d5119d5b1bbbe199dc2260d19eade59df907e27d
PeterMorrison <morrisonman56@gmail.com>
2019-11-30 19:44:28 -0700

    try removing () from include

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'].'app\includes\header.inc.php');
-require_once($_SERVER['DOCUMENT_ROOT'].'app\includes\hamburger.inc.php');
+require_once "includes\header.inc.php";
+require_once "includes\hamburger.inc.php";
 ?>
 
 
