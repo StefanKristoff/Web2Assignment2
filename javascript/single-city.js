93f40841c5656b496f1bd903617f3a59308a3bcc
PictureBookGraduate <smora050@mtroyal.ca>
2019-11-30 22:00:40 -0700

    citiy JS + Travel Photos

---
+++ javascript/single-city.js
@@ -0,0 +1,143 @@
+
+document.addEventListener("DOMcontentloaded", function () {
+//fetch URL for the city data
+const cityEndpoint = 'http://localhost/web2Assignment2/api-cities.php'
+let cities = retieveCityStorage();
+let citiesWithImages = retrieveCityWithImagesStorage();
+
+// Fetch from url and call function
+fetchCities();
+
+// Calls to create country list, create listener, etc
+
+function updateCityStorage() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+}
+
+function retieveCityStorage(){
+    return JSON.parse(localStorage.getItem("cities")) || [];
+}
+
+function updateCityWithStorage(){
+    localStorage.setItem("citiesWithImages",  JSON.stringify(citiesWithImages));
+}
+function retrieveCityWithImagesStorage(){
+    return JSON.parse(localStorage.getItem("citiesWithImages")) || [];
+}
+
+// Fetch the cities from the endPoint
+function fetchCities(){
+    let search = cityEndpoint + "?id=ALL";
+    
+    if (cities.length < 1){
+        fetch(search)
+            .then(response => response.json())
+            .then(data => {
+                for (let c of data){
+                    cities.push(c);
+                }
+                updateCityStorage();
+            })
+            .catch(error => console.error(error));
+    }
+}
+
+function setCityOnClickListener() {
+    const list = document.querySelector("#citylist");
+    list.addEventListener("click", e => {
+        if (e.target && e.target.nodeName == "LI"){
+            pupulateCityDetails(e.target.textContent);
+            populateCityListImages(e.target.textContent);
+        }
+    });
+}
+
+function populateCityList(countryName){
+    emptyCityList();
+    let countrySelected = countries.find(c => c.name == countryName);
+    selectedCountry = countryName;
+
+    const suggested = document.querySelector("#filteredCity");
+
+    suggested.innerHTML = "";
+
+    selectedCities = cities.filter(obj => {
+        const regex = new RegExp(countrySelected.iso, "gi");
+        return obj.iso.match(regex);
+    })
+
+    selectedCities = selectedCities.sort((a, b) => {
+        return a.name < b.name ? -1 : 1;
+    })
+
+    selectedCities.forEach(city => {
+        var option = document.createElement('li');
+        option.textContent = city.name;
+        suggestions.appendChild(option);
+    });
+
+    if (selectedCities.length == 0){
+        suggestions.textContent = "NO cities found";
+    }
+}
+
+function populateCityListImages(){
+    const suggestions = document.querySelector("#filteredCity");
+        emptyCityList();
+        let countrySelected = countries.find(c => c.name == countryName);
+        selectedCountry = countryName;
+
+        let imageCities = citiesWithImages.filter(obj => {
+            const regex = new RegExp(countrySelected.iso, "gi");
+            return obj.iso.match(regex);
+        });
+
+
+        imageCities = imageCities.sort((a, b) => {
+            return a.name < b.name ? -1 : 1;
+        })
+
+        imageCities.forEach(city => {
+            var option = document.createElement('li');
+            option.textContent = city.name;
+            suggestions.appendChild(option);
+        });
+}
+    // Shows cities that have name matching what is typed by users
+    function displayCityMatches() {
+        const suggestions = document.querySelector("#filteredCity");
+        const cityMatches = findCityNameMatches(this.value, selectedCities);
+
+        suggestions.innerHTML = "";
+
+        cityMatches.forEach(city => {
+            var option = document.createElement('li');
+            option.textContent = city.name;
+            suggestions.appendChild(option);
+        });
+    }
+
+    // Sets the click listener for the "Only have Images" button for cities
+    function setCityHasImagesListener() {
+        const btn = document.querySelector("#cityHasImages");
+        btn.addEventListener("click", function () {
+            let text = document.querySelector(".city .search");
+            text.textContent = "";
+            text.value = "";
+            populateCityListHasImages(selectedCountry);
+        });
+    }
+
+    // Resets the filters so no input is given
+    function resetCityFilters() {
+        let text = document.querySelector(".city .search");
+        text.textContent = "";
+        text.value = "";
+        populateCityList(selectedCountry);
+    }
+
+function emptyCityList(){
+    document.querySelector("#filteredCity").innerHTML = "";
+}
+
+})
