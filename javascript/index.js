d98c21d18b07cae81fb048206de8d565f73e77f9
Peter Morrison <morrisonman56@gmail.com>
2019-12-05 18:58:49 -0700

    Started login stuff

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log('DOM loaded');
+
+
+    // document.querySelector("#travelImages").addEventListener("mouseleave", e => {
+    //     if (e.target && e.target.nodeName != "IMG") {
+    //         document.querySelector("#myPopup").style.display = "none";
+    //     }
+    // });
+});
--- login.php
+++ login.php
@@ -23,19 +23,26 @@ require_once "includes/hamburger.inc.php";
         createHamburger();
         ?>
 
-        <section class='nested hero-img'>
-            <h3>WELCOME</h3>
-            <div class='innerNested'>
+        <!-- <section class='nested hero-img'> -->
+            <h3>LOGIN</h3>
+            <!-- <div> -->
+                <form>
+                    <label>Email:</label>
+                    <input type="text" />
 
-                <div>
+                    <label>Password:</label>
+                    <input type="password" />
+                </form>
+
+                <!-- <div>
                     <a class="btn" href="http://localhost/Web2Assignment2/login.php"><button type='button'>Login</button></a>
                 </div>
                 <div>
                     <button type='button'>Join</button>
-                </div>
-                <input type="text" placeholder="SEARCH BOX FOR PHOTOS...">
-            </div>
-        </section>
+                </div> -->
+                <!-- <input type="text" placeholder="SEARCH BOX FOR PHOTOS..."> -->
+            <!-- </label> -->
+        <!-- </section> -->
     </main>
 </body>
 
