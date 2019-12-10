d0f161674dc82cc10481ee0552b7a6243e9f4402
Peter Morrison <morrisonman56@gmail.com>
2019-12-09 23:59:19 -0700

    Fixed logout to keep favourites, helped with favourite image display

--- favourites.php
+++ favourites.php
@@ -6,19 +6,21 @@ require_once 'config.inc.php';
 
 $pdo = setConnectionInfo(DBCONNSTRING, DBUSER, DBPASS);
 $info = [];
+$imagelist = [];
 session_start();
 if(isset($_SESSION['add'])){
     $ids = $_SESSION['add'];
     foreach($ids as $id){
         $singleImage = getImageByID($pdo, $id);
         $index= $singleImage;
+        array_push($imagelist, $singleImage);
     }
 }
 if(isset($_POST['remove'])){
     unset($_SESSION['add']);
 }
 $info[] = $index;
-print($info);
+// print($info);
 
 
 // for ($i = 0; $i < sizeof($info); $i++) {
@@ -28,11 +30,11 @@ print($info);
 //     echo "</br>";
 // }
 
-    foreach($info as $e){
-        // print_r($e);
-        $img = $e['Path'];
-        echo $img;
-    }
+    // foreach($info as $e){
+    //     // print_r($e);
+    //     $img = $e['Path'];
+    //     echo $img;
+    // }
 
 
 ?>
@@ -59,10 +61,20 @@ print($info);
             <?php
             print_r($ids);
             print_r(sizeof($ids));
-            for ($i = 0; $i < 10; $i++) {
-                echo "<img height='250px' width='250px' src='fakeimg.jpg'/>";
+            // for ($i = 0; $i < 10; $i++) {
+            //     echo "<img height='250px' width='250px' src='fakeimg.jpg'/>";
+            // }
+            // print_r($imagelist);
+            foreach($imagelist as $img) {
+                // print_r($img[0]['ImageID']);
+                foreach($img as $i) {
+                    print_r($i['ImageID']);
+                    $idWant = $i['Path'];
+                    ?>
+                    <img height='150px' width='150px' src='images\case-travel-master\images\square150\<?php echo strtolower($idWant); ?>'>
+                    <?php 
+                }
             }
-            
 
             ?>
             
